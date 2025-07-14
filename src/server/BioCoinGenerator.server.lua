7f32865f66d3bb1cae54aeecf99ff2d8feebeaf4
RosendoTorres321 <rosendot321@gmail.com>
2025-07-13 22:51:39 -0400

    add biocoin logic

---
+++ src/server/BioCoinGenerator.server.lua
@@ -0,0 +1,11 @@
+-- BioCoinGeneratorScript
+local coinAmount = 10
+local cooldown = 3
+
+while true do
+    wait(cooldown)
+    local coinDrop = Instance.new("IntValue")
+    coinDrop.Name = "BioCoinDrop"
+    coinDrop.Value = coinAmount
+    coinDrop.Parent = workspace
+end
